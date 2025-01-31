a974dbeed72eea13c6328fc7889e899ae301aaa8
Jeyrio <J.atueyi@yahoo.com>
2025-01-31 19:06:30 +0100

    chores: making changes file Completed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" making changes """
+
+def makeChange(coins, total):
+    """ return the minimal change """
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse=True)
+    num_change = 0
+    sum_change = 0
+    
+    for a in coins:
+        while sum_change < total:
+            sum_change += a
+            num_change += 1
+        if sum_change == total:
+            return num_change
+        sum_change -= a
+        num_change -= 1
+    return -1
