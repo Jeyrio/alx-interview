6e7362c1f380c77cb7cd5ba249220e912ba7b29e
Jeyrio <J.atueyi@yahoo.com>
2025-02-01 16:01:11 +0100

    fix: pycodestyle styntax in making changes file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ making changes """
 
+
 def makeChange(coins, total):
     """ return the minimal change """
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     num_change = 0
     sum_change = 0
-    
+
     for a in coins:
         while sum_change < total:
             sum_change += a
