dc4ff752f4b0f20af581f3720de9900f2f58bdac
Jeyrio <J.atueyi@yahoo.com>
2025-02-03 16:16:40 +0100

    refactor: restructure the code written in the island perimeter file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
+"""
+Module for island perimeter
+"""
 
 
 def island_perimeter(grid):
+    # Determine the number of rows and columns in the grid
+    rows = len(grid)
+    columns = len(grid[0])
+
+    # Intialize the perimeter variable to 0
     perimeter = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                perimeter += 4
-            # print(perimeter)
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
+    # Lopp through each cell in the grid
+    for a in range(rows):
+        for b in range(columns):
+            if grid[a][b] == 1:
+                # Check the top edge
+                if 1 == 0 or grid[a-1][b] == 0:
+                    perimeter += 1
 
-                if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 1
+                # Check the bottom edge
+                if a == rows-1 or grid[a + 1][b] == 0:
+                    perimeter += 1
 
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
+                # Check the left edge
+                if b == 0 or grid[a][b-1] == 0:
+                    perimeter += 1
 
-                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1
+                # Check the right edge
+                if b == columns-1 or grid[a][b+1] == 0:
+                    perimeter += 1
+
+    # Return the total perimeter
     return perimeter
