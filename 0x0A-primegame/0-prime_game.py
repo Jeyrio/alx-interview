c2f37b41ccf24258e2c7285c49ddb8700f7291bf
Jeyrio <J.atueyi@yahoo.com>
2025-02-04 15:33:07 +0100

    refactor: change the code structures

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,100 @@
 #!/usr/bin/python3
+"""
+Prime Game Module
+"""
 
 
-def sieve_of_eratosthenes(n):
-    # Generate prime numbers up to n using the Sieve of Eratosthenes.
-    # Args: n (int): The upper limit to find primes within.
-    # returns: list: A list of prime numbers up to n.
-
-    # intialize a boolean list to find prime numbers up to n
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False # 0 and 1 are not primes
-    for i in range(2, int(n**0.5) + 1):
-        if primes[i]:
-            for j in range(i*i, n + 1, i):
-                primes[j] = False
-    return [i for i in range(2, n + 1) if primes[i]]
+def is_prime(n):
+    """Check if a number is prime"""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
-def simulate_round(n):
-    # Simulate a single round of the game.
-    primes = sieve_of_eratosthenes(n)
-    if not primes:
-        return "Ben"  # If there are no primes, Ben wins
-    return "Maria" if len(primes) % 2 == 1 else "Ben"
+def get_prime_numbers(n):
+    """Get all prime numbers up to n using Sieve of Eratosthenes"""
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+
+    primes = []
+    for i in range(2, n + 1):
+        if sieve[i]:
+            primes.append(i)
+    return primes
+
+
+def simulate_game(n):
+    """
+    Simulate a single game with maximum number n
+    Returns: Winner of the game ('Maria' or 'Ben')
+    """
+    if n < 2:
+        return 'Ben'
+
+    # Get all numbers from 1 to n
+    numbers = set(range(1, n + 1))
+    primes = get_prime_numbers(n)
+
+    # Simulate the game
+    maria_turn = True
+    while primes:
+        # Find the smallest available prime
+        current_prime = None
+        for prime in primes:
+            if prime in numbers:
+                current_prime = prime
+                break
+
+        if not current_prime:
+            break
+
+        # Remove the prime and its multiples
+        multiples = set(range(current_prime, n + 1, current_prime))
+        numbers -= multiples
+        primes.remove(current_prime)
+
+        # Switch turns
+        maria_turn = not maria_turn
+
+    # If it's Maria's turn and no moves left, Ben wins
+    # If it's Ben's turn and no moves left, Maria wins
+    return 'Ben' if maria_turn else 'Maria'
 
 
 def isWinner(x, nums):
-    # Determine the winner of multiple rounds of the game.
-    if x <= 0 or not nums:
+    """
+    Determine the winner of x rounds of the game
+    Args:
+        x: number of rounds
+        nums: array of n values for each round
+    Returns:
+        Name of the player that won the most rounds
+        None if winner cannot be determined
+    """
+    if not nums or x < 1:
         return None
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        winner = simulate_round(n)
-        if winner == "Maria":
+
+    maria_wins = ben_wins = 0
+
+    for n in nums[:x]:
+        if n is None:
+            continue
+        winner = simulate_game(n)
+        if winner == 'Maria':
             maria_wins += 1
         else:
             ben_wins += 1
+
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        return 'Ben'
+    return None
